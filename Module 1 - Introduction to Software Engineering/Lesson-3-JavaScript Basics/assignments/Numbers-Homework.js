9b87925394894a9befc0d8575c21390756684507
Ecochic7 <planet7even@gmail.com>
2023-12-13 20:18:00 -0500

    completed Numbers hw

---
+++ Module 1 - Introduction to Software Engineering/Lesson-3-JavaScript Basics/assignments/Numbers-Homework.js
@@ -0,0 +1,74 @@
+//SUBMIT A LINK TO THE GITHUB REPOSITORY CONTAINING THIS ASSIGNMENT
+// Remember to console.log() to debug your code and check your solutions!
+
+
+// Declare a variable and assign it to a numeric value
+// Half that value and re-assign it to the previous variable 
+
+const num1 = 4
+console.log(num1 - 2)
+
+// Create two negative numbers, assign them to variables and subtract one from the other together 
+
+num2 = -7
+num3 = -11 
+console.log( num2 - num3)
+
+// Create three numbers, assign them to variables and multiply them together 
+
+num4 = 5
+num5 = 10 
+num6 = 15
+console.log(num4 * num5 * num6)
+
+// Create two even numbers, assign them to variables and divide one from the other 
+
+num7 = 12
+num8 = 14
+console.log(num7 / num8)
+
+// Create two odd numbers, assign them to variables and find the remainder of dividing one by the other 
+
+num9 = 13
+num10 = 15
+console.log(num9 % num10)
+
+// find the minimum value in a set of numbers: 10,20,30,40,50 
+
+let minvalue = Math.min(10,20,30,40,50)
+console.log(minvalue)
+
+// find the maximum value in a set of numbers: -1,-2,-3,-4,-5 
+
+let maxvalue = Math.max(-1,-2,-3,-4,-5)
+console.log(maxvalue)
+
+// Create a variable and find it's remainder, if divided by 2
+
+const num11 = 8 % 2
+console.log(num11)
+
+// find the square root of 144 
+
+let squatRoot = Math.sqrt(144)
+console.log(squatRoot)
+
+// find the value of 3 to the power of 7 
+
+let valuepow = Math.pow(3, 7)
+console.log(valuepow)
+
+// find the value of 73.2 rounded 
+
+let valueround = Math.round(73.2)
+console.log(valueround)
+
+// find the rounded-down value of 9.9 
+
+let valueround2 = Math.round(9.9)
+console.log(valueround2)
+
+// find the absolute value of -1231
+
+let absvalue = Math.abs(-1231)
+console.log(absvalue)

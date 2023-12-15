62a56f02a7595b5d04ff9c280dc878b4d0e33aa9
Ecochic7 <planet7even@gmail.com>
2023-12-15 17:45:20 -0500

    Boolean hw

---
+++ Module 1 - Introduction to Software Engineering/Lesson-3-JavaScript Basics/assignments/Boolean-Homework.js
@@ -0,0 +1,147 @@
+//1. Write a statement that checks if 2 and 'two' are equal
+const equal = 2 === 'two'
+console.log(equal)
+//2. Write a statement that checks if '2' and 2 are loosely equal
+const equal1 = '2' != 2
+console.log(equal)
+//3. Write a statement that checks if 14 and '14' are strictly equal
+const equal2 = 14 === '14'
+console.log(equal2)
+//4. Write a statement that checks if 10 is greater than -100
+const great = 10 > -100
+console.log(great)
+//5. Write a statement that checks if 4 is strictly equal to the rounded value of 4.4
+const equal3 = 4 === Math.round() * 4.4
+console.log(equal3)
+//6. Write a statement that checks if 10 is greater than or equal to 9.1 rounded up
+const equal4 = 10 > Math.ceil() * 9.1 || 10 === Math.ceil() * 9.1
+console.log(equal4)
+
+//7. Write a statement that checks if 10 is greater than 2 and 3 is greater than 4
+const equal5 = 10 > 2 && 3 > 4
+console.log(equal5)
+// // 7.a Without changing any of the numbers, or their order. How do we make this print true?
+const equal6 = 10 != 2 && 3 != 4
+console.log(equal6)
+
+
+//8. Pick a number between 1-20 and assign it to a variable
+const fruit = "Watermelons"
+console.log(fruit)
+const howmany = Math.random() * 20
+console.log(howmany)
+// // This is the number of watermelons you purchased.
+// // If your number of watermelons is 1 or 2, console.log 'Looks like you like watermelon'
+// // If your number of watermelons is more than 2, console.log 'Thats a lot of watermelon'
+// // If your number of watermelons is more than 5, print, 'Thats too many watermelon!'
+// // If your number of watermelons is more than 10, print, 'You must be CRAZY for watermelon!'
+// // If your number of watermelons is more than 15, print 'Did you have a coupon?'
+
+if(howmany == 1 || howmany == 2){
+  console.log ('Looks like you like watermelon')
+}
+else if(howmany > 2){
+    console.log ('Thats a lot of watermelon')
+}
+else if(howmany > 5){
+    console.log ('Thats too many watermelon!')
+}
+else if(howmany > 10){
+    console.log ('You must be CRAZY for watermelon!')
+}
+else {
+    console.log ('Did you have a coupon?')
+}
+//   if(numOfWM === 1 || numOfWM ===  2) {
+//     console.log('Looks like you like watermelon')
+//   } else if (numOfWM > 2 && numOfWM <= 5) {
+//     console.log('Thats a lot of watermelon')
+//   } else if (numOfWM > 5 && numOfWM <= 10){
+//     console.log('Thats too many watermelon!')
+//   } else if (numOfWM > 10 && numOfWM <= 15) {
+//     console.log('You must be CRAZY for watermelon!')
+//   } else {
+//     console.log('Did you have a coupon?')
+//   }
+
+// //9. Create a variable that represents your current hunger on a level of 1-10
+//   // Using a ternary, if your hunger is greater than 6 print, 'Time to eat!'
+//   // Otherwise print. "I'm not hungry"
+const myHunger = Math.random() * 10
+console.log(myHunger)
+ let howhungry = 6 > myHunger? 'Time to eat' : "I'm not hungry"
+ console.log(howhungry)
+// 10. Think of an Magic-8 Ball
+// Write a switch statement that works the same way.
+// When you run this file you should
+// Generate a random number
+// And get an out put from your switch statement?
+// Ex:
+// I update a question variable to say "Will I win the lottery?"
+// I run node <FILENAME>
+// The console prints out:
+// Will I win the lottery? Not Likely.
+const Magic8 = "Magic-8 Ball"
+const lottery = Math.random() * 8
+
+switch(lottery){
+    case 1:
+        console.log(`${Magic8} Will I will the lottery? Not Likely`);
+        break;
+    case 2:
+        console.log(`${Magic8} Will I will the lottery? Not Likely`);
+        break;
+     case 3:
+        console.log(`${Magic8} Will I will the lottery? Not Likely`);
+        break;
+    case 4:
+        console.log(`${Magic8} Will I will the lottery? Not Likely`);
+        break;
+    case 5:
+        console.log(`${Magic8} Will I will the lottery? Not Likely`);
+        break;
+     case 6:
+        console.log(`${Magic8} Will I will the lottery? Not Likely`);
+        break;
+    case 7:
+        console.log(`${Magic8} Will I will the lottery? Not Likely`);
+        break;
+     default:
+            console.log(`${Magic8} Will I will the lottery? Yess`);
+            
+}
+// BONUS:
+// On the prior example, try and have the answer print like someone is speaking to you.
+const tom = "How are You?"
+console.log(tom)
+switch(tom){
+    default:
+    console.log(`Tom asked me. ${tom}`)
+    
+
+}
+// Ex:
+//Input:  Will I win the lottery?
+//Output: Will you win the lottery? Not likely. <- Notice I has been programmatically changed to you
+const input = "Will I win the lottery?"
+console.log(input)
+switch(input){
+default:
+    console.log(`I asked a friend ${input} and they answered Will you win the lottery? Not likely.`)
+    
+
+}
+
+
+// Ex2:
+// Input: Will I get a new pet this year?
+// Output: Will you get a pet this year? It is certain.
+const input1 = "Will I get a new pet this year?"
+console.log(input1)
+switch(input1){
+default:
+    console.log(`I asked myself should I or  ${input1} I thought about it then I answeed myself I Will get a pet this year? It is certain.`)
+    
+
+}
+// Try and handle many edge cases - can we put in any question and have it spit out a grammatically correct response?
--- Module 1 - Introduction to Software Engineering/Lesson-3-JavaScript Basics/assignments/String-Assignment.js
+++ Module 1 - Introduction to Software Engineering/Lesson-3-JavaScript Basics/assignments/String-Assignment.js
@@ -3,7 +3,8 @@
 // 1. Concatenate "hello" with "goodbye"
  const string1 = "hello"
  const string2  = "goodbye"
-console.log(string1.concat(string2))
+ const Concatenate = `${string1} ${string2}`
+console.log(Concatenate)
 // 2. Save the substring "Friend" from the string "Hello Friend"
 const greeting = "Hello Friend"
 console.log(greeting.slice(6, 12))
@@ -42,21 +43,23 @@ console.log(initials)
 
 // 5. Get the 5th character from the following string: 'Hello World'
 const greeting1 = 'Hello World'
-console.log(greeting1.slice(5))
+const fifth = greeting[4]
+console.log(fifth)
 // 6. Find the index of the dash from 'Coding-Time!'
 const coding = 'Coding-Time!'
 console.log(coding.indexOf("-"))
 // 7. Write code to grab the final letter from a any string.
 const string = "string"
-console.log(string.length - 1)
+const index = string.length - 1
+console.log(index)
 // 8. Write code to grab the first 3 letters from a string. 
 const string3 = "letters"
 console.log(string3.slice(0,3))
 
 // 9. Turn the following string into an array of words: 'This-is-a-sentence';
 const sentence1 = 'This-is-a-sentence'
-console.log(sentence1.split(''))
+console.log(sentence1.split('-'))
 // 10. Make the string of 12 from the following two numbers:
 let one = 1;
 let two = 2;
-console.log(one,two)
+console.log`${one}${two}`

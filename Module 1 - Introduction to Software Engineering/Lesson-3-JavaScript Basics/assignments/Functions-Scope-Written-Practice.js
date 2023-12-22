e56d294f16c82fb2cb0561b66d1087ea5efad8ca
Ecochic7 <planet7even@gmail.com>
2023-12-21 22:20:41 -0500

    Function Scope hw

---
+++ Module 1 - Introduction to Software Engineering/Lesson-3-JavaScript Basics/assignments/Functions-Scope-Written-Practice.js
@@ -0,0 +1,22 @@
+// Answer the following questions in your own words
+// 1. What is a function?
+// A function in JavaScript is similar to a procedure—a set of statements that performs a task or calculates a value
+// 2. Why is it useful?
+//  performs a task or calculates a value
+// 3. Give a 'real world example'. Ours was wood chipper. Try and think of something yourself. 
+    // function open door return close door
+// 4. There is different syntax / ways of defining a function write out the syntax for each:
+// // A. Function declaration:
+function greeting ()
+// // B. Function expressions:
+ function greeting(name)
+// // C. Arrow functions: 
+greeting(name) => {}
+// 5. What is a parameter?
+a parameter is the variable use to in the funtion
+// 6. What is an argument?
+  an argument is what you tell the function to do
+// 7. What does the return statement do?
+    a return statement is what you want the function to say
+// 8. What is scope? Do you have a real-world example of something similar to scope?
+
